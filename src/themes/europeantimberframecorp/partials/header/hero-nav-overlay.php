e2829f7b374bf9e76e2d9ee659ede233e5169de5
charllab <developer.charl@gmail.com>
2022-08-04 11:08:54 -0700

    wrapped up desktop hero-image

--- src/themes/europeantimberframecorp/partials/header/hero-nav-overlay.php
+++ src/themes/europeantimberframecorp/partials/header/hero-nav-overlay.php
@@ -1,40 +1,9 @@
 <header id="header" class="hero-nav-overlay position-relative z-index-1">
 
-    <div class="mainnav-m collapse navbar-collapse position-relative z-index-500" id="mobile-nav">
-        <div class="container">
-            <div class="row justify-content-center">
-                <?php wp_nav_menu([
-                    'theme_location' => 'primary',
-                    'container_class' => 'col-10',
-                    'container_id' => 'mobile-nav-container',
-                    'menu_class' => 'navbar-nav ml-auto d-lg-none',
-                    'fallback_cb' => '',
-                    'menu_id' => 'main-menu',
-                    'walker' => new understrap_WP_Bootstrap_Navwalker(),
-                ]); ?>
-
-                <div class="col-10">
-                    <a class="btn btn-link text-white px-0"
-                       href="tel:<?php echo strip_tel(get_field('phone_number', 'options')); ?>"><?php the_field('phone_number', 'options'); ?></a>
-
-                    <div class="social-links">
-                        <?php while (have_rows('social_links', 'options')): the_row(); ?>
-                            <a class="social-link text-white" target="_blank" href="<?php the_sub_field('url'); ?>">
-                                <i class="<?php the_sub_field('icon_class'); ?>">
-                                    <span class="sr-only"><?php the_sub_field('label'); ?></span>
-                                </i>
-                            </a>
-                        <?php endwhile; ?>
-                    </div>
-                </div><!-- col -->
-            </div><!-- row -->
-        </div><!-- container -->
-    </div><!-- #mobile-nav -->
-
     <nav class="navbar navbar-expand-lg navbar-dark">
 
-        <div class="container d-flex justify-content-between align-items-start">
-            <div class="nav-logo position-relative" style="margin-left: -4px;">
+        <div class="container d-flex justify-content-between align-items-start position-relative">
+            <div class="nav-logo position-relative z-index-100" style="margin-left: -4px;">
                 <a href="<?php echo esc_url(home_url('/')); ?>">
                     <?php get_template_part('partials/header/svg-logo-grey'); ?>
                     <span class="sr-only"><?php bloginfo('name'); ?></span>
@@ -44,12 +13,14 @@
                         </svg>
                     </span>
                 </a>
-            </div>
+            </div><!-- nav-logo -->
 
-            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target=".mainnav-m"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
+
+                <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target=".mainnav-m"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+
 
             <div class="d-none d-lg-flex mt-lg-1 mainnav">
 
@@ -77,12 +48,52 @@
         </div>
     </nav>
 
+    <div class="mainnav-m collapse navbar-collapse position-relative d-lg-none" id="mobile-nav">
+
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container d-flex justify-content-between align-items-start position-relative">
+                <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target=".mainnav-m"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+            </div>
+        </nav>
+
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <?php wp_nav_menu([
+                    'theme_location' => 'primary',
+                    'container_class' => 'col-10',
+                    'container_id' => 'mobile-nav-container',
+                    'menu_class' => 'navbar-nav ml-auto',
+                    'fallback_cb' => '',
+                    'menu_id' => 'main-menu',
+                    'walker' => new understrap_WP_Bootstrap_Navwalker(),
+                ]); ?>
+
+                <div class="col-10">
+
+                    <div class="social-links">
+                        <?php while (have_rows('social_links', 'options')): the_row(); ?>
+                            <a class="social-link text-white" target="_blank" href="<?php the_sub_field('url'); ?>">
+                                <i class="<?php the_sub_field('icon_class'); ?>">
+                                    <span class="sr-only"><?php the_sub_field('label'); ?></span>
+                                </i>
+                            </a>
+                        <?php endwhile; ?>
+                    </div>
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+    </div><!-- #mobile-nav -->
+
 
     <?php
     if (is_front_page()) :
         get_template_part('partials/header/heroslider');
     else :
-        echo 'Not the frontpage';
+        get_template_part('partials/header/heroimage');
     endif;
     ?>
 
---
+++ src/themes/europeantimberframecorp/partials/header/heroimage.php
@@ -0,0 +1,28 @@
+<section id="hero-image" class="position-relative">
+    <?php $heroimageurl = get_field('hero_image'); ?>
+    <div class="block__tint-overlay position-absolute z-index-1"></div>
+    <div class="is-full-height position-relative bg-dark"
+         style="background: #3050A0 url(<?php echo $heroimageurl['sizes']['large']; ?>) no-repeat center center; background-size: cover;">
+        <div class="hero-content">
+        <div class="container position-relative z-index-100">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-md-6 position-relative">
+                    <div class="pl-lg-350 py-250">
+                        <h1 class="h3 text-white text-capitalize font-weight-bold position-relative pr-1 z-index-100">Timber Frame Beauty built to last for generations</h1>
+                        <div class="underlay-svg z-index-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 2600 114">
+                                <path id="Rectangle_157" data-name="Rectangle 157" d="M0,0H2595a5,5,0,0,1,5,5V109a5,5,0,0,1-5,5H0a0,0,0,0,1,0,0V0A0,0,0,0,1,0,0Z" opacity="0.502"/>
+                            </svg>
+                        </div><!-- underlay-svg -->
+                    </div><!-- pl -->
+                </div><!-- col -->
+                <div class="col-md-4 col-lg-3">
+                    <div class="pr-lg-2">
+                        <p class="text-white font-weight-bold">A Canadian EuroTimber Cabana Kit</p>
+                    </div><!-- pr -->
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
+        </div><!-- hero-content -->
+    </div><!-- is-full-height -->
+</section><!-- #hero-image -->
